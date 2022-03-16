48e381ab200518c3d209a9ef17d86643b2651157
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-16 20:01:41 +0300

    initialized votes for each nominee

---
+++ app/models.py
@@ -0,0 +1,8 @@
+#first we will tke input of what nominnee we want to keep
+nominee1 = input("Enter the name of the 1st nominee:")
+nominee2 = input("Enter the name of the 2nd nominee:")
+
+#initial vote count for both must be 0
+
+nm1_votes = 0
+nm2_votes = 0
