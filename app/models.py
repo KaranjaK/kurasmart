ab9a17b8ca0d2ed5338db395bb0efa8e1541e244
vyonna <vyonna.njenga@student.moringaschool.com>
2022-03-16 20:20:35 +0300

    added if,elif and else function to loop

--- app/models.py
+++ app/models.py
@@ -16,7 +16,23 @@ while True:
     if voter in voter_id:
         print("you are a voter")
         voter_id.remove(voter) #we will remove so that again voter can't vote
+        print("------------------------------------")
         print("to give vote to ",nominee1,"press 1 ")
         print("to give vote to ",nominee2,"press 2 ")
+        print("------------------------------------")
+        vote = int(input("Enter your vote : "))
+        if vote == 1:
+            nm1_votes +=1
+            print(nominee1,"Thanks you for voting for them !!")
+        elif vote == 2:
+            nm2_votes +=1
+            print(nominee2,"Thanks you for voting for them !!")
+        elif vote > 2:
+            print("Check your pressed key !!")
+        else:
+            print("You are not a voter or have already voted !!")    
+
+
+
 
 
